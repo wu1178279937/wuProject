17bc511ac131274e1ff18bcc8696d50504d85719
wujun <wujun@tuandai.com>
2017-04-13 11:01:34 +0800

    modules

---
+++ app/src/main/java/com/example/et/wuproject/view/MyViewPager.java
@@ -0,0 +1,19 @@
+package com.example.et.wuproject.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+
+/**
+ * @author: wujun
+ * @Filename:
+ * @Description:
+ * @Copyright: Copyright (c) 2017 Tuandai Inc. All rights reserved.
+ * @date: 2017/4/13 10:58
+ */
+public class MyViewPager extends ViewPager {
+    public MyViewPager(Context context) {
+        super(context);
+    }
+
+
+}
